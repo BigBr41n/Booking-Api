2ba4d5f59345e011865b652d2240a6d8c1d72845
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-07-01 20:02:38 +0200

    cal , iDget , getAll , booking services

--- src/services/booking.services.ts
+++ src/services/booking.services.ts
@@ -20,12 +20,12 @@ export const createBooking = async (
 ): Promise<Booking> => {
   try {
 
-    //const totalPrice = await calculateTotalPrice(bookingData);
+    const totalPrice = await calculateTotalPrice(bookingData);
 
     const newBooking = await prisma.booking.create({
       data: {
         ...bookingData,
-      //  totalPrice,
+        totalPrice,
         userId : userId,
         status: BookingStatus.PENDING,
       },
@@ -79,3 +79,193 @@ export const updateBookingDetails = async (
 };
 
 
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Cancels a booking.
+ * @param {string} bookingId - The ID of the booking to cancel.
+ * @returns {Promise<Booking>} The canceled booking.
+ * @throws Will throw an error if the booking cannot be canceled.
+ */
+export const cancelBooking = async (bookingId: string): Promise<Booking> => {
+    try {
+      return await prisma.booking.update({
+        where: { id: bookingId },
+        data: { status: BookingStatus.CANCELLED },
+      });
+    } catch (err: any) {
+      logger.error("Error during cancel booking service:", err);
+      if (err instanceof ApiError) {
+        throw err;
+      }
+      throw new ApiError("Failed to cancel booking", 500);
+    }
+  };
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /**
+   * Retrieves booking history for a user.
+   * @param {string} userId - The ID of the user.
+   * @returns {Promise<Booking[]>} The list of bookings for the user.
+   * @throws Will throw an error if the booking history cannot be retrieved.
+   */
+  export const getBookingHistory = async (userId: string): Promise<Booking[]> => {
+    try {
+      return await prisma.booking.findMany({
+        where: { userId },
+      });
+    } catch (err: any) {
+      logger.error("Error during get booking history service:", err);
+      if (err instanceof ApiError) {
+        throw err;
+      }
+      throw new ApiError("Failed to fetch booking history", 500);
+    }
+  };
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /**
+   * Automatically updates booking status .
+   * @param {string} bookingId - The ID of the booking.
+   * @param {BookingStatus} status - The new status of the booking.
+   * @returns {Promise<Booking>} The updated booking.
+   * @throws Will throw an error if the booking status cannot be updated.
+   */
+  export const updateBookingStatus = async (
+    bookingId: string,
+    status: BookingStatus
+  ): Promise<Booking> => {
+    try {
+      return await prisma.booking.update({
+        where: { id: bookingId },
+        data: { status },
+      });
+    } catch (err: any) {
+      logger.error("Error during update booking status service:", err);
+      if (err instanceof ApiError) {
+        throw err;
+      }
+      throw new ApiError("Failed to update booking status", 500);
+    }
+  };
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /**
+   * Calculates the total price for a booking.
+   * @param {Omit<Booking, "id" | "createdAt" | "updatedAt" | "status">} bookingData - The data for the booking.
+   * @returns {Promise<number>} The total price.
+   * @throws Will throw an error if the total price cannot be calculated.
+   */
+  const calculateTotalPrice = async (
+    bookingData: Omit<Booking, "id" | "createdAt" | "updatedAt" | "status">
+  ): Promise<number> => {
+  
+  
+    let totalPrice = 0;
+    if (bookingData.flightId) {
+      const flight = await prisma.flight.findUnique({
+        where: { id: bookingData.flightId },
+      });
+      if (flight) totalPrice += flight.price;
+    }
+    if (bookingData.hotelId) {
+      const hotel = await prisma.hotel.findUnique({
+        where: { id: bookingData.hotelId },
+      });
+      if (hotel) totalPrice += hotel.price;
+    }
+    return totalPrice;
+  };
+  
+  
+  
+  
+  
+  
+  
+  /**
+   * Retrieves a booking by ID.
+   * @param {string} bookingId - The ID of the booking.
+   * @returns {Promise<Booking | null>} The booking with the specified ID.
+   * @throws Will throw an error if the booking cannot be retrieved.
+   */
+  export const getBookingById = async (
+    bookingId: string
+  ): Promise<Booking | null> => {
+    try {
+      return await prisma.booking.findUnique({
+        where: { id: bookingId },
+      });
+    } catch (err: any) {
+      logger.error("Error during get booking by ID service:", err);
+      if (err instanceof ApiError) {
+        throw err;
+      }
+      throw new ApiError("Failed to fetch booking by ID", 500);
+    }
+  };
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /**
+   * Retrieves all bookings for a user by userID.
+   * @param {string} userId - The ID of the user
+   * @returns {Promise<Booking[]>} The list of all bookings.
+   * @throws Will throw an error if the bookings cannot be retrieved.
+   */
+  export const getAllBookings = async (userId: string): Promise<Booking[]> => {
+    try {
+      const allBookings = await prisma.booking.findMany({
+        where: { userId },
+      });
+      return allBookings;
+    } catch (err: any) {
+      logger.error("Error during get all bookings service:", err);
+      if (err instanceof ApiError) {
+        throw err;
+      }
+      throw new ApiError("Failed to fetch all bookings", 500);
+    }
+  };
+  
